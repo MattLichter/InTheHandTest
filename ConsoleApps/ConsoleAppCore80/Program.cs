7c7f689e3a7067d90421c1d8e5eb7f8b186156ca
Matt Lichter <lichter@bainet.com>
2024-10-29 12:13:47 -0400

    Run console test to scan for devices (fail).
    ConnectException: No path specified for UNIX transport

--- ConsoleApps/ConsoleAppCore80/Program.cs
+++ ConsoleApps/ConsoleAppCore80/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace ConsoleAppCore80
+﻿using InTheHand.Bluetooth;
+
+namespace ConsoleAppCore80
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            var devices = Bluetooth.ScanForDevicesAsync().Result;
+            Console.WriteLine($"Found {devices.Count} devices");
+            foreach (var device in devices)
+            {
+                Console.WriteLine($"Name: {device.Name} Id: {device.Id}");
+            }
         }
     }
 }
