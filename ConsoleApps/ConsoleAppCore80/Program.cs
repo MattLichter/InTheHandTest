666ba1edc4fdd43f91458469a5eab68d022c5288
Matt Lichter <lichter@bainet.com>
2024-10-29 12:11:39 -0400

    Add console app for .NET Core 8.0.

---
+++ ConsoleApps/ConsoleAppCore80/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppCore80
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
