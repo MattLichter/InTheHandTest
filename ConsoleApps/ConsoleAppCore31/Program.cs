0f270f1a7cf307074d5352b08297f2a332fede2e
Matt Lichter <lichter@bainet.com>
2024-10-29 10:39:53 -0400

    Add console app for .NET Core 3.1.

---
+++ ConsoleApps/ConsoleAppCore31/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppCore31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
