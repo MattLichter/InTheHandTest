0385cc1494f1ac005b59e0ba900670f94d71e76c
Matt Lichter <lichter@bainet.com>
2024-10-29 12:03:18 -0400

    Run console test to scan for devices (succeed).

--- ConsoleApps/ConsoleAppFramework/Program.cs
+++ ConsoleApps/ConsoleAppFramework/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InTheHand.Bluetooth;
+using System;
 
 namespace ConsoleAppFramework
 {
@@ -7,6 +8,12 @@ namespace ConsoleAppFramework
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var devices = Bluetooth.ScanForDevicesAsync().Result;
+            Console.WriteLine($"Found {devices.Count} devices");
+            foreach (var device in devices)
+            {
+                Console.WriteLine($"Name: {device.Name} Id: {device.Id}");
+            }
             Console.ReadKey();
         }
     }
